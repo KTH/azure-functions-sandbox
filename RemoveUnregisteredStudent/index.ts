49d5b3ce8af191713745ab6cba02dfc7f5edfeca
Carlos Saito <carlos@saito.style>
2022-03-04 11:52:27 +0100

    Parse XML message from LIS Adapter

--- RemoveUnregisteredStudent/index.ts
+++ RemoveUnregisteredStudent/index.ts
@@ -1,7 +1,17 @@
 import { AzureFunction, Context } from "@azure/functions"
+import { XMLParser } from "fast-xml-parser";
 
-const serviceBusTopicTrigger: AzureFunction = async function(context: Context, mySbMsg: any): Promise<void> {
-    context.log('ServiceBus topic trigger function processed message', mySbMsg);
+const serviceBusTopicTrigger: AzureFunction = async function(context: Context, message: string): Promise<void> {
+    // context.log('ServiceBus topic trigger function processed message', mySbMsg);
+
+    const parser = new XMLParser();
+    const jsonObj = parser.parse(message);
+
+    const activityRoundId = jsonObj["ns0:membershipRecord"]["ns0:membership"]["ns0:collectionSourcedId"];
+    const studentId = jsonObj["ns0:membershipRecord"]["ns0:membership"]["ns0:member"]["ns0:personSourcedId"];
+
+    context.log(activityRoundId, studentId)
+
 };
 
 export default serviceBusTopicTrigger;
