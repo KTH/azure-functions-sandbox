b40f84f7fd6406ac2970b00e52831ead8d08ef06
Sebastian Ware <sebastian@urbantalk.se>
2022-03-15 16:23:48 +0100

    Use assert for param checking and apply strict types to existing code

--- RemoveUnregisteredStudent/index.ts
+++ RemoveUnregisteredStudent/index.ts
@@ -1,25 +1,25 @@
 import { AzureFunction, Context } from "@azure/functions"
+import { assert } from "console";
 import { XMLParser } from "fast-xml-parser";
+import { TKthId, TActivityRoundId, TEnrollmentId } from './types';
 import { getKthId } from "./ug";
 
 const serviceBusTopicTrigger: AzureFunction = async function(context: Context, message: string): Promise<void> {
     // context.log('ServiceBus topic trigger function processed message', mySbMsg);
 
+    // 1. Parse incomming mesage
     const parser = new XMLParser();
     const jsonObj = parser.parse(message);
 
-    const activityRoundId = jsonObj?.["ns0:membershipRecord"]?.["ns0:membership"]?.["ns0:collectionSourcedId"];
+    const activityRoundId = new TActivityRoundId(jsonObj?.["ns0:membershipRecord"]?.["ns0:membership"]?.["ns0:collectionSourcedId"]);
     const studentId = jsonObj?.["ns0:membershipRecord"]?.["ns0:membership"]?.["ns0:member"]?.["ns0:personSourcedId"];
 
-    if (!activityRoundId || !studentId) {
-        context.log("This message doesn't include either activity round id or student id. Skipping...")
-        return;
-    }
+    assert(activityRoundId, "Message is missing 'activityRoundId'");
+    assert(studentId, "Message is missing 'studentId'");
 
+    // 2. Call UG to get KTH ID
     const kthId = await getKthId(studentId);
-
-    context.log(activityRoundId, studentId, kthId);
-
+    // context.log(activityRoundId, studentId, kthId);
 };
 
 export default serviceBusTopicTrigger;
